8d2cca845da0e75296e689e6b66c0c64af29d8cb
rings-s <rings9619@gmail.com>
2024-09-22 02:52:05 -0400

    the whole updates before data fetching

--- front_e-comm/tailwind.config.js
+++ front_e-comm/tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        warning: "var(--warning)",
+
       },
     },
   },
